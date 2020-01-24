88f4e1133ab6bf83f00feb5a8cd6bf788a53f46c
dldydrhkd <sksda4614@naver.com>
2020-01-24 18:34:08 +0900

    Update 3049.cpp

--- ~10000/3049.cpp
+++ ~10000/3049.cpp
@@ -6,6 +6,6 @@ int main() {
 	cin >> n;
 	cout << n * (n - 1)*(n - 2)*(n - 3) / 24;
 
-	//교점을 만들려면 4개의 점이 필요하므로
-	//결국 사각형을 만들 수 있는개수가 답이다
+	//you can make a dot by a rectangle
+	//so the answer is how many rectangle can make
 }
