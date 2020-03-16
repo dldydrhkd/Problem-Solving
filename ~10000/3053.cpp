2d72de0801b86c11e9159506128035c91c8754db
dldydrhkd <48857296+dldydrhkd@users.noreply.github.com>
2020-03-16 15:16:12 +0900

    Create 3053.cpp

---
+++ ~10000/3053.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <cmath>
+#define _USE_MATH_DEFINES
+using namespace std;
+
+
+
+int main(){
+        int r;
+        cin >>r;
+        double uclid=r*r*M_PI;
+        double taxi=r*r*2;
+
+        printf("%.6f\n%.6f",uclid, taxi);
+
+}
