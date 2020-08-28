b8ac600fae1bcac9cc1222d2fd87f6722ae3fafd
dldydrhkd <48857296+dldydrhkd@users.noreply.github.com>
2020-08-29 02:30:17 +0900

    이건 꼭 풀어야 해!

---
+++ 백준/10000~20000/17390.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int arr[300001];
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n,q;
+    cin >>n>>q;
+    for(int i=1; i<=n; i++){
+        cin >>arr[i];
+    }
+    sort(arr, arr+n+1);
+    for(int i=1; i<=n; i++){
+        arr[i]+=arr[i-1];
+    }
+    while(q--){
+        int l,r;
+        cin >>l>>r;
+        cout<<arr[r]-arr[l-1]<<"\n";
+    }
+}
