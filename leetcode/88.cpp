1b611446aed31b9d3e52bb24ddcaa791c4e0babc
dldydrhkd <48857296+dldydrhkd@users.noreply.github.com>
2021-11-12 14:46:42 +0900

    Merge Sorted Array
    정렬

--- leetcode/88.cpp
+++ leetcode/88.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> v;
-        for(int i=0; i<nums.size(); i++){
-            for(int j=i+1; j<nums.size(); j++){
-                if(nums[i]+nums[j]==target){
-                    v.push_back(i);
-                    v.push_back(j);
-                }
-            }
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for(int i=m; i<m+n; i++){
+            nums1[i] = nums2[i-m];
         }
-        return v;
+        sort(nums1.begin(), nums1.end());
     }
 };
