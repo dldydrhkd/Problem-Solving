94b468d38d605906ca1c63b6f9641ca0b9548ec8
이용광 <sksda4614@naver.com>
2022-01-10 18:46:36 +0900

    Non-overlapping Intervals
    그리디

--- leetcode/435.java
+++ leetcode/435.java
@@ -1,44 +1,27 @@
 class Solution {
-    public static int num = 1;
-    public static int x = 0;
-    public static int y = 0;
-    Solution(){
-        num = 1;
-        x = 0;
-        y = 0;
-    }
-    public void fill(int[][] arr, int n, int len){
-        if(num==n*n){
-            arr[x][y] = num;
-            return;
-        }
-        else if(num>n*n){
-            return;
-        }
-        int[] dir_x = {0,1,0,-1};
-        int[] dir_y = {1,0,-1,0};
-        for(int i=0; i<4; i++){
-            for(int j=0; j<len-1; j++){
-                int temp_x = x + dir_x[i]*j;
-                int temp_y = y + dir_y[i]*j;
-                if(j==0){
-                    arr[x][y] = num;
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, new Comparator<int[]>(){
+            public int compare(int[] o1, int[] o2){
+                if(o1[1]==o2[1]){
+                    return o2[0]-o1[0];
                 }
                 else{
-                    arr[temp_x][temp_y] = num;
+                    return o1[1]-o2[1];
                 }
-                num+=1;
             }
-            x+=dir_x[i]*(len-1);
-            y+=dir_y[i]*(len-1);
+        });
+        int cnt = 0;
+        int idx = 1;
+        int right = intervals[0][1];
+        while(idx<intervals.length){
+            if(right<=intervals[idx][0]){
+                right=intervals[idx][1];
+            }
+            else{
+                cnt++;
+            }
+            idx++;
         }
-        x+=1;
-        y+=1;
-        fill(arr,n,len-2);
-    }
-    public int[][] generateMatrix(int n) {
-        int[][] arr = new int[n][n];
-        fill(arr, n, n);
-        return arr;
+        return cnt;
     }
 }
