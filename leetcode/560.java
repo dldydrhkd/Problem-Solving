493fd0d6f559e0a3351777a576d331493e0b769c
이용광 <sksda4614@naver.com>
2022-01-13 23:48:43 +0900

    Subarray Sum Equals K
    해쉬맵 O(n), prefix_sum (n^2) 가능

---
+++ leetcode/560.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        for(int i=1; i<nums.length; i++){
+            nums[i] +=nums[i-1];
+        }
+        int cnt = 0;
+        for(int i=0; i<nums.length; i++){
+            for(int j=i; j<nums.length; j++){
+                int sum = 0;
+                if(i==0){
+                    sum = nums[j];
+                }
+                else{
+                    sum = nums[j]-nums[i-1];
+                }
+                if(sum==k){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
