0e3a03006281bab0c79d9638d9bb2ef1e9a758a0
이용광 <sksda4614@naver.com>
2022-01-27 23:09:37 +0900

    카드2
    큐

---
+++ 백준/~10000/2164.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    queue<int> q;
+    for (int i = 1; i <= n; i++) {
+        q.push(i);
+    }
+
+    while (q.size() != 1) {
+        q.pop();
+        int temp = q.front();
+        q.pop();
+        q.push(temp);
+    }
+
+    cout << q.front();
+
+}
