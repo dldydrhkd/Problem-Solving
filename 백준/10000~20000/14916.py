f98d543ee11b852312be0a01dcc6d47f34819c15
Volc <48857296+dldydrhkd@users.noreply.github.com>
2025-02-10 23:55:50 +0900

    거스름돈
    dp, bottom up

---
+++ 백준/10000~20000/14916.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
+
+n = int(input())
+
+li = [-1 for i in range(100001)]
+li[0] = 0
+x = [5,2]
+
+for i in range(1,n+1):
+    if li[i-x[0]] >= 0 and li[i-x[1]] >= 0:
+        li[i] = min(li[i-x[0]], li[i-x[1]])+1
+    elif li[i-x[0]] >= 0:
+        li[i] = li[i-x[0]]+1
+    elif li[i-x[1]] >= 0:
+        li[i] = li[i-x[1]]+1
+    print(i, li[i])
+
+print(li[n])
